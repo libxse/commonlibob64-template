593eacac308d142f841f8ae410e2216d16eca7b9
Qudix <17361645+Qudix@users.noreply.github.com>
2026-01-11 18:49:48 -0600

    feat: update lib - no longer using lock file

--- xmake.lua
+++ xmake.lua
@@ -1,31 +1,22 @@
 -- set minimum xmake version
-set_xmakever("2.8.2")
+set_xmakever("3.0.0")
 
--- includes
+-- include subprojects
 includes("lib/commonlibob64")
 
--- set project
+-- set project constants
 set_project("commonlibob64-template")
 set_version("0.0.0")
 set_license("GPL-3.0")
-
--- set defaults
 set_languages("c++23")
 set_warnings("allextra")
 
--- set policies
-set_policy("package.requires_lock", true)
-
--- add rules
+-- add common rules
 add_rules("mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 
--- targets
+-- define targets
 target("commonlibob64-template")
-    -- add dependencies to target
-    add_deps("commonlibob64")
-
-    -- add commonlibob64 plugin
     add_rules("commonlibob64.plugin", {
         name = "commonlibob64-template",
         author = "libxse",
